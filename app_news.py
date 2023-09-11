5a6ad24a8ee5a80e7ffa53b26c28132f5655c090
elr9 <90631728+elr9@users.noreply.github.com>
2023-09-11 10:37:01 -0600

    Update app_news.py

--- app_news.py
+++ app_news.py
@@ -63,7 +63,7 @@ def main():
 
     # Mexico News
     st.subheader("Mexico News")
-    response = fetch_headlines(newsapi_key, country="mx")
+    response = fetch_headlines(newsapi_key, country=mx)
     articles = response.get("articles", [])
     for idx, article in enumerate(articles):
         button_key = f"technology-{idx}-{article['url']}"
